f863398b6ccf9396e644154d8b8064e5102b7e83
teligramboturl <teligramboturl@gmail.com>
2025-10-16 12:10:59 +0530

    Update BOT_NAME in config.py

--- modules/config.py
+++ modules/config.py
@@ -2,13 +2,14 @@
 import os
 
 # Bot Configuration
-BOT_NAME = os.environ.get('BOT_NAME', 'NANA_LTUW_BOT')
+BOT_NAME = os.environ.get('BOT_NAME', 'palsab')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 API_ID = int(os.environ.get('API_ID', ''))
 API_HASH = os.environ.get('API_HASH', '')
 MONGO_URI = os.environ.get('MONGO_URI', '')
 
 # Additional Configuration
-OWNER_ID = int(os.environ.get('OWNER_ID', '504848353'))
-LOG_CHANNEL_ID = int(os.environ.get('LOG_CHANNEL_ID', '-1002971022913'))
+OWNER_ID = int(os.environ.get('OWNER_ID', ''))
+LOG_CHANNEL_ID = int(os.environ.get('LOG_CHANNEL_ID', ''))
+
 
