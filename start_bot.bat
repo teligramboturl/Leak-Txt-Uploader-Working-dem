1f6598ddda4935ba5985e47faf619093d63515bd
teligramboturl <teligramboturl@gmail.com>
2025-10-16 12:17:31 +0530

    Update Owner ID in start_bot.bat

--- start_bot.bat
+++ start_bot.bat
@@ -3,7 +3,7 @@ echo Starting Telegram Bot...
 echo.
 echo Bot Configuration:
 echo - Token: 7626013382:AAEvmqpHw4otevswmJbtSw7yK03rGYVmc4M
-echo - Owner ID: 7792539085
+echo - Owner ID: 6677821706
 echo - API ID: 22746239
 echo.
 echo Starting bot process...
